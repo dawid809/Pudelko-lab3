6215d7630b49eed5b8e3d92dceecd7d1b0a2a278
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2020-11-19 10:19:47 +0100

    Adding a constructor for the box

--- PudelkoApp/Program.cs
+++ PudelkoApp/Program.cs
@@ -6,7 +6,7 @@ namespace PudelkoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
         }
     }
 }
--- PudelkoLib/Class1.cs
+++ PudelkoLib/Class1.cs
@@ -1,8 +1,109 @@
 ﻿using System;
+using System.Globalization;
 
-namespace PudelkoLib
+namespace PudelkoLibrary
 {
-    public class Class1
+    public enum UnitOfMeasure
     {
+        milimeter = 1000,
+        centimeter = 100,
+        meter = 1
+    }
+
+    public sealed class Pudelko //zapieczętowanie klasy za pomocą sealed
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public double c { get; set; }
+
+        public double Am { get => this.a; }
+        public double Bm { get => this.b; }
+        public double Cm { get => this.c; }
+
+        public double Acm { get => this.a * 0.1; }
+        public double Bcm { get => this.b * 0.1; }
+        public double Ccm { get => this.c * 0.1; }
+
+        public double Amm { get => this.a * 0.001; }
+        public double Bmm { get => this.b * 0.001; }
+        public double Cmm { get => this.c * 0.001; }
+
+        public double A
+        {
+            get => this.Am;
+            private set
+            {
+                this.a = SetVal(value);
+            }
+        }
+        public double B
+        {
+            get => this.Bm;
+            private set
+            {
+                this.b = SetVal(value);
+            }
+        }
+        public double C
+        {
+            get => this.Cm;
+            private set
+            {
+                this.c = SetVal(value);
+            }
+        }
+
+        private double SetVal(double value)
+        {
+            switch (Unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    value *= 0.001;
+                    break;
+                case UnitOfMeasure.centimeter:
+                    value *= 0.01;
+                    break;
+                case UnitOfMeasure.meter:
+                    break;
+            }
+            var tmp = Math.Round(value, 3);
+            if (tmp <= 0 || tmp > 10)
+                throw new ArgumentOutOfRangeException("zle dane");
+
+            return value;
+        }
+
+        public UnitOfMeasure Unit { get; set; } = UnitOfMeasure.meter;
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public string ToString(UnitOfMeasure unit)
+        {
+            return base.ToString();
+        }
+
+        private double GetDefault()
+        {
+            switch (Unit)
+            {
+                case UnitOfMeasure.milimeter:
+                    return 100;
+                case UnitOfMeasure.centimeter:
+                    return 10;
+                default:
+                    return 0.1D;
+            }
+        }
+
+        public Pudelko(double? a = null, double? b = null, double? c = null, UnitOfMeasure unit = UnitOfMeasure.meter)
+        {
+            this.Unit = unit;
+            this.A = (a != null) ? (double)a : GetDefault();
+            this.B = (b != null) ? (double)b : GetDefault();
+            this.C = (c != null) ? (double)c : GetDefault();
+        }
     }
 }
