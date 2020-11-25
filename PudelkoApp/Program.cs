792705773b933cc6eafbfef3541e50f60035dc65
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2020-11-25 14:56:30 +0100

    Changes in constructor and adding  representation ToString().

--- PudelkoApp/Program.cs
+++ PudelkoApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PudelkoLibrary;
+using System;
 
 namespace PudelkoApp
 {
@@ -6,7 +7,18 @@ namespace PudelkoApp
     {
         static void Main(string[] args)
         {
-        
+            Pudelko pudelko = new Pudelko(2.5,9.321,0.1, UnitOfMeasure.meter);
+            //Pudelko p2 = new Pudelko(1, 1, 1, UnitOfMeasure.milimeter);
+            //Console.WriteLine(p2.Objetosc);
+            
+
+            Console.WriteLine(pudelko.ToString("m"));
+            Console.WriteLine(pudelko.ToString("cm"));
+            Console.WriteLine(pudelko.ToString("mm"));
+            
+
+
+
         }
     }
 }
--- PudelkoLib/Class1.cs
+++ PudelkoLib/Class1.cs
@@ -10,47 +10,38 @@ namespace PudelkoLibrary
         meter = 1
     }
 
-    public sealed class Pudelko //zapieczętowanie klasy za pomocą sealed
+    public sealed class Pudelko : IFormattable  //zapieczętowanie klasy za pomocą sealed
     {
-        public double a { get; set; }
-        public double b { get; set; }
-        public double c { get; set; }
+        public  double a { get; }
+        public  double b { get;  }
+        public  double c { get;  }
 
         public double Am { get => this.a; }
         public double Bm { get => this.b; }
         public double Cm { get => this.c; }
 
-        public double Acm { get => this.a * 0.1; }
-        public double Bcm { get => this.b * 0.1; }
-        public double Ccm { get => this.c * 0.1; }
+        public double Acm { get => this.a / 0.01; }
+        public double Bcm { get => this.b / 0.01; }
+        public double Ccm { get => this.c / 0.01; }
 
-        public double Amm { get => this.a * 0.001; }
-        public double Bmm { get => this.b * 0.001; }
-        public double Cmm { get => this.c * 0.001; }
+        public double Amm { get => this.a / 0.001; }
+        public double Bmm { get => this.b / 0.001; }
+        public double Cmm { get => this.c / 0.001; }
 
         public double A
         {
             get => this.Am;
-            private set
-            {
-                this.a = SetVal(value);
-            }
+            
         }
         public double B
         {
             get => this.Bm;
-            private set
-            {
-                this.b = SetVal(value);
-            }
+           
         }
         public double C
         {
             get => this.Cm;
-            private set
-            {
-                this.c = SetVal(value);
-            }
+            
         }
 
         private double SetVal(double value)
@@ -75,35 +66,45 @@ namespace PudelkoLibrary
 
         public UnitOfMeasure Unit { get; set; } = UnitOfMeasure.meter;
 
-        public override string ToString()
-        {
-            return base.ToString();
-        }
-
-        public string ToString(UnitOfMeasure unit)
-        {
-            return base.ToString();
-        }
-
-        private double GetDefault()
-        {
-            switch (Unit)
-            {
-                case UnitOfMeasure.milimeter:
-                    return 100;
-                case UnitOfMeasure.centimeter:
-                    return 10;
-                default:
-                    return 0.1D;
-            }
-        }
 
         public Pudelko(double? a = null, double? b = null, double? c = null, UnitOfMeasure unit = UnitOfMeasure.meter)
         {
             this.Unit = unit;
-            this.A = (a != null) ? (double)a : GetDefault();
-            this.B = (b != null) ? (double)b : GetDefault();
-            this.C = (c != null) ? (double)c : GetDefault();
+            this.a = (a != null) ? SetVal ((double)a) : 0.1;
+            this.b = (b != null) ? SetVal ((double)b) : 0.1;
+            this.c = (c != null) ? SetVal ((double)c) : 0.1;
         }
+
+        public override string ToString()
+        {
+            return this.ToString("m", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider provider = null)
+        {
+            if (String.IsNullOrEmpty(format)) format = "m";
+            if (provider == null) provider = CultureInfo.CurrentCulture;
+
+            switch (format)
+            {
+                case "m":
+                    return $"{Am.ToString("0.000", provider)} m \u00D7 " +
+                        $"{Bm.ToString("0.000", provider)} m \u00D7 " +
+                        $"{Cm.ToString("0.000", provider)} m";
+                case "cm":
+                    return $"{Acm.ToString("000.0".TrimStart(new Char[] { '0' }), provider)} cm \u00D7 " +
+                       $"{Bcm.ToString("000.0".TrimStart(new Char[] { '0' }), provider)} cm \u00D7 " +
+                       $"{Ccm.ToString("000.0".TrimStart(new Char[] { '0' }), provider)} cm";
+                case "mm":
+                    return $"{Amm.ToString("0000".TrimStart(new Char[] { '0' }), provider)} mm \u00D7 " +
+                       $"{Bmm.ToString("0000".TrimStart(new Char[] { '0' }), provider)} mm \u00D7 " +
+                       $"{Cmm.ToString("0000".TrimStart(new Char[] { '0' }), provider)} mm";
+                default:
+                    throw new FormatException("wrong code");
+            }
+        }
+            //Property pole i objetosc
+        public double Objetosc { get => Math.Round( A * B * C, 9); }
+        public double Pole { get => Math.Round(2 * A * B + 2 * A * C + 2 * B * C, 6); }
     }
 }
